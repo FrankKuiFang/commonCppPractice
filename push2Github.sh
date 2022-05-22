ecf0aed727084e1decdfd607893824419784495e
Frank <fdfanggang@163.com>
2022-05-22 22:19:15 +0800

    change script

---
+++ push2Github.sh
@@ -0,0 +1,7 @@
+
+# push latest master to corresponding github repo.
+
+#git remote add githubRepo git@github.com:FrankKuiFang/commonCppPractice.git
+#git push -u githubRepo master
+
+git push -u git@github.com:FrankKuiFang/commonCppPractice.git master
