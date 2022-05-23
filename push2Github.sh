6da30850f1431d13f9492a1c2e489a6f49a0d99b
Frank <fdfanggang@163.com>
2022-05-23 22:02:05 +0800

    fix push2Github.sh

--- push2Github.sh
+++ push2Github.sh
@@ -2,7 +2,7 @@
 # push latest master to corresponding github repo.
 
 #git remote add githubRepo git@github.com:FrankKuiFang/commonCppPractice.git
-#git push -u githubRepo master
+#git push githubRepo master
 
-git push -u git@github.com:FrankKuiFang/commonCppPractice.git master
+git push git@github.com:FrankKuiFang/commonCppPractice.git master
 #@@@@
